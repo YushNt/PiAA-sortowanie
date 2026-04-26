318511f02144b19310b3af80b597fd940535a2ca
Kornel <kornel.okinczyc@gmail.com>
2026-04-26 18:35:55 +0200

    optymalizacja wyboru pivota - mediana z początku, srodka i końca

--- include/algorithms/quicksort.h
+++ include/algorithms/quicksort.h
@@ -9,15 +9,23 @@ class QuickSort
 {
 public:
     void sort(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end){
-        if (start < end)
+        if (end - start > 1)
         {
-            auto pivot_loc = partition(start, end);
-            sort(start, pivot_loc);
+            auto pivot_loc = partition(start, end); //podział na dwie części + pivot_loc - iterator wskazujący na element, który jest już na swoim miejscu
+            sort(start, pivot_loc); //rekurnecja 
             sort(pivot_loc + 1, end);
         }
     };
 private:
     typename std::vector<T>::iterator partition(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end){
+        auto mid = start + (end - start) / 2;
+        auto last = end - 1;
+        //optymalizacja pivota - mediana z początku, środka i końca
+        if (*mid < *start) std::iter_swap(mid, start);
+        if (*last < *start) std::iter_swap(last, start);
+        if (*last < *mid) std::iter_swap(last, mid);
+        std::iter_swap(start, mid);
+
         auto pivot = *start;
         auto leftwall = start;
         for (auto i = start + 1; i < end; ++i){
